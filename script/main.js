a29c2f13d2b293cfa037c8e2f793026d294f8378
Ricardogol <ricardinhofilho008@gmail.com>
2026-05-30 15:11:00 -0300

    Estados e times no js

---
+++ script/main.js
@@ -0,0 +1,47 @@
+const estadoBusca = document.querySelector('#estado-busca');
+const dataBusca = document.querySelector('#data-busca');
+const clubeBusca = document.querySelector('#clube-busca');
+const botaoBusca = document.querySelector('#botao-busca');
+const dataBusca= document.querySelector('#data-busca');
+/* buscaEstados */{
+//Sul
+const buscaPR= ['Athletico', 'Coritiba', 'Operário', 'Londrina'];
+const buscaRS= ['Internacional', 'Grêmio', 'Juventude', 'Caxias', 'Ypiranga', 'Brasil de Pelotas', 'Guarany de Bagé', 'São José', 'São Luiz'];
+const buscaSC= ['Chapecoense', 'Avai', 'Criciuma', 'Barra', 'Brusque', 'Figueirense', 'Blumenau', 'Joinville', 'Marcílio dias', 'Santa catarina'];
+//Sudeste
+const buscaES= ['Rio Branco', 'Vitoria', 'Real Noroeste'];
+const buscaMG= ['Atlético', 'Cruzeiro', 'América mineiro', 'Athletic', 'Betim', 'Democrata GV', 'Pouso Alegre', 'Tombense', 'Uberlândia'];
+const buscaRJ= ['Botafogo', 'Flamengo', 'Fluminense', 'Vasco da gama', 'Volta Redonda', 'America', 'Madureira', 'Maricá', 'Nova iguaçu', 'Portuguesa', 'Sampario Corrêa'];
+const buscaSP= ['Corinthians', 'Palmeiras', 'Bragantino', 'Santos', 'São Paulo', 'Mirassol','Botafogo', 'Guarani', 'Ituano', 'Novorizontino', 'Ponte Preta', 'Ferroviaria', 'São Bernardo', 'Água Santa', 'Inter de Limeira', 'Oeste', 'Portuguesa', 'Santo André', 'Velo Clube'];
+//Centro-Oeste
+const buscaDF= ['Brasiliense', 'Capital-DF'];
+const buscaGO= ['Atlético-GO', 'Goiás', 'Vila Nova', 'Anápolis', 'Abecat', 'Aparecidense', 'CRAC', 'Goiatuba', 'Inhumas'];
+const buscaMS= ['Ivinhema', 'Operário-MS'];
+const buscaMT= ['Cuiabá', 'Luverdense', 'Mixto', 'União Rondonópolias'];
+//Nordeste
+const buscaAL= ['CRB', 'ASA', 'CSA', 'CSE'];
+const buscaBA= ['Bahia', 'Vitória'];
+const buscaCE= ['Atlético de Alagoinhas', 'Jacuipense', 'Juazeirense', 'Porto'];
+const buscaMA= ['MAC', 'Sampaio Corrêa', 'Moto Club', 'Imperatriz', 'IAPE'];
+const buscaPB= ['Botafogo-PB', 'Serra Branca', 'Sousa', 'Treze'];
+const buscaPE= ['Sport', 'Nautico', 'Central de Caruaru', 'Retrô', 'Santa Cruz'];
+const buscaPI= ['Altos', 'Fluminense-PI', 'Parnahyba', 'Piauí'];
+const buscaRN= ['ABC', 'América-RN', 'Laguna'];
+const buscaSE= ['Confiança', 'Sergipe'];
+//Norte
+const buscaAC= ['Humaitá', 'Independência'];
+const buscaAM= ['Amazonas', 'Manauara', 'Manaus', 'Nacional-AM'];
+const buscaAP= ['Trem', 'Oratório'];
+const buscaPA= ['Remo', 'Paysandu', 'Águia de Marabá', 'Tuna Luso'];
+const buscaRR= ['GAS', 'Monte Roraima', 'São Raimundo'];
+const buscaRO= ['Porto Velho', 'Guaporé'];
+const buscaTO= ['Araguaína', 'Tocantinópolis'];
+}
+
+function render() {
+
+}
+
+function buscaClube() {
+    
+}
